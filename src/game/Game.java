3dec3912548380a073ea93cf31e1da69cc3dacaf
TaylorDoud <taylor.doud@edu.sait.ca>
2016-10-04 16:53:42 -0600

    Commented out ERRORS

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,8 +2,106 @@ package game;
 
 public class Game {
 
-	public Game() {
-		// TODO Auto-generated constructor stub
+	public Game() 
+	{
+		
 	}
 
+//	private void checkWinCondition()
+//	{
+//		String winMessage = "";
+//		//Player 1 win condition
+//		
+//		if(button1.getText().equals("X")  && button2.getText().equals("X") && button3.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button4.getText().equals("X")  && button5.getText().equals("X") && button6.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button7.getText().equals("X") && button8.getText().equals("X") && button9.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button1.getText().equals("X") && button4.getText().equals("X") && button7.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button2.getText().equals("X") && button5.getText().equals("X") && button8.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button3.getText().equals("X") && button6.getText().equals("X") && button9.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button1.getText().equals("X") && button5.getText().equals("X") && button9.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button3.getText().equals("X") && button5.getText().equals("X") && button7.getText().equals("X"))
+//		{
+//			winMessage = "Player one Wins";
+//			setButtonsInactive();
+//		}
+//		
+//		//player 2 Win Conditions
+//		
+//		
+//		if(button1.getText().equals("O")  && button2.getText().equals("O") && button3.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button4.getText().equals("O")  && button5.getText().equals("O") && button6.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button7.getText().equals("O") && button8.getText().equals("O") && button9.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button1.getText().equals("O") && button4.getText().equals("O") && button7.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button2.getText().equals("O") && button5.getText().equals("O") && button8.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button3.getText().equals("O") && button6.getText().equals("O") && button9.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button1.getText().equals("O") && button5.getText().equals("O") && button9.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(button3.getText().equals("O") && button5.getText().equals("O") && button7.getText().equals("O"))
+//		{
+//			winMessage = "Player two Wins";
+//			setButtonsInactive();
+//		}
+//		else if(!button1.isEnabled() && !button3.isEnabled() && !button4.isEnabled() && !button5.isEnabled() && !button6.isEnabled() && !button7.isEnabled() && !button8.isEnabled() && !button9.isEnabled())
+//		{
+//			winMessage = "CATS GAME";
+//		}
+//		winner.setText(winMessage);
+//	}
+//	
+	
 }
